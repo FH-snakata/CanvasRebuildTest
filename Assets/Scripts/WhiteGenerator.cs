774a4cbb725c745b52ee40e856be46881bd5250a
snakata <majime.nekodanshi@gmail.com>
2018-09-30 00:46:20 +0900

    処理負荷を計測するための準備

---
+++ Assets/Scripts/WhiteGenerator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class WhiteGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private int _instanceNum = 1000;
+
+    [SerializeField]
+    private GameObject _white = null;
+
+    private void Awake()
+    {
+        for (var i = 0; i < _instanceNum; i++)
+        {
+            Instantiate(_white, transform);
+        }
+    }
+
+}
